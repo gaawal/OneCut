db0575d184e99bf7344a9513a6b934da5fa5c2d8
jiahualink <840132699@qq.com>
2024-07-18 23:48:30 +0800

    添加微博场景下使用本地素材的场景

--- app/services/material.py
+++ app/services/material.py
@@ -248,3 +248,14 @@ def search_videos_pixabay(search_term: str,
         logger.error(f"search videos failed: {str(e)}")
 
     return []
+
+
+def get_local_videos(audio_duration, video_clip_duration):
+    # 计算需要的视频片段总个数
+    video_counts = audio_duration // video_clip_duration
+    # 获取缓存目录中的视频文件列表
+    video_files = [os.path.join(utils.cache_videos_dir(), file) for file in os.listdir(utils.cache_videos_dir()) if
+                   file.endswith('.mp4')]
+    # 随机选择所需数量的视频文件
+    selected_videos = random.sample(video_files, video_counts)
+    return selected_videos
--- app/services/task.py
+++ app/services/task.py
@@ -91,9 +91,13 @@ async def start(task_id, redis_state, params: VideoParams, request: Request):
 
             images_files = await get_images_files(request=request, params=params)
             await save_task_state(redis_state, task_id, TaskState.PROCESSING, 50, TaskDetailState.DOWNLOADING_VIDEOS, draft)
-
-            downloaded_videos = await material.download_videos(task_id, video_terms, params.video_source, params.video_aspect, params.video_concat_mode, audio_duration, params.video_clip_duration, redis_state, draft, utils.task_dir(task_id))
-
+            downloaded_videos = []
+            if params.weibo_mid:
+                logger.info("微博话题模式，视频素材从本地获取")
+                downloaded_videos = material.get_local_videos(audio_duration, params.video_clip_duration)
+            if not downloaded_videos:
+                downloaded_videos = await material.download_videos(task_id, video_terms, params.video_source, params.video_aspect, params.video_concat_mode, audio_duration, params.video_clip_duration, redis_state, draft, utils.task_dir(task_id))
+            logger.success("获取视频素材文件为：", downloaded_videos)
             if not subtitle_path:
                 await save_task_state(redis_state, task_id, TaskState.FAILED, 50, TaskFailureReason.FAILED_GENERATING_SUBTITLE, draft, TaskFailureReason.FAILED_GENERATING_SUBTITLE)
                 return
