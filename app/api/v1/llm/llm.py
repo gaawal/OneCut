0befe13ed922193317dec221425fc695d305b491
jiahualink <840132699@qq.com>
2024-07-19 08:22:24 +0800

    添加微博热搜详情是否显示已采集逻辑

--- app/api/v1/llm/llm.py
+++ app/api/v1/llm/llm.py
@@ -68,6 +68,52 @@ async def generate_video_script_and_terms(request: Request, body: VideoScriptReq
     return Success(data=response)
 
 
+@router.post("/scripts_terms", response_model=VideoScriptResponse, summary="为视频创建脚本以及获取对应的关键词")
+async def generate_video_script_and_terms(request: Request, body: VideoScriptRequest):
+    if (body.weibo_url and body.weibo_mid):
+        # 如果要从微博热搜获取微博信息
+        logger.info(f"开始获取实时微博热搜话题 {body.weibo_url} 【{body.weibo_title}】")
+        redis_service = get_redis_service(request)
+        cached_data = await redis_service.get(REDIS_HOT_ARTICLE_KEY.format(body.weibo_mid))  # 使用 await 关键字调用异步方法
+        if cached_data:
+            logger.success(f"获取微博热搜话题【{body.weibo_title}】缓存成功")
+            weibo_article_data = json.loads(cached_data)
+        else:
+            weibo_article_data = await fetch_hot_article(body.weibo_mid, body.weibo_url)
+            logger.success(f"实时获取微博热搜话题:{body.weibo_title} 成功")
+            # 将数据缓存到 Redis 1天更新一次
+            await redis_service.set(REDIS_HOT_ARTICLE_KEY.format(body.weibo_mid),
+                                    json.dumps(weibo_article_data, ensure_ascii=False),
+                                    expire=RedisExpireTime.ONE_DAY)
+            hot_data = await redis_service.get(REDIS_HOTSEARCH_KEY)
+            if hot_data:
+                hot_data = json.loads(hot_data)
+                for i, item in enumerate(hot_data):
+                    if body.weibo_mid == item.get('mid'):
+                        hot_data[i]["collect_status"] = True
+            await redis_service.set(REDIS_HOTSEARCH_KEY, json.dumps(hot_data, ensure_ascii=False),
+                                    expire=RedisExpireTime.THIRTY_MINUTES)
+
+            logger.success(f"{body.weibo_mid} 微博热搜话题 {body.weibo_title} 保存redis成功.")
+        # 获取微博数据内容条数 影响ai分析微博内容
+        get_content_counts = 5
+        weibo_summary = generate_weibo_summary(weibo_article_data, get_content_counts)  # 假设需要获取5条评论
+        # 把微博热搜作为视频主题输入
+        body.video_subject = weibo_summary
+    video_script, video_terms, video_title = llm.generate_script_and_terms(video_subject=body.video_subject,
+                                                                           language=body.video_language,
+                                                                           paragraph_number=body.paragraph_number,
+                                                                           video_category=body.video_category,
+                                                                           amount=body.amount,
+                                                                           word_count=body.word_count)
+    response = {
+        "video_title": video_title,
+        "video_script": video_script,
+        "video_terms": video_terms
+    }
+    return Success(data=response)
+
+
 @router.post("/inspire_scripts_terms", response_model=VideoScriptResponse, summary="通过文案灵感类别为视频创建脚本以及获取对应的关键词")
 async def generate_video_script_and_terms_by_inspire(request: Request, body: VideoScriptRequest):
     logger.info("Generating video by inspire，body is {}".format(body))
