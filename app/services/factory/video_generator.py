e2a2d18017d70b2a9c30f4a208736cc896223711
jiahualink <840132699@qq.com>
2024-07-20 21:45:00 +0800

    优化封面逻辑，生成的封面带有符号支持换行

--- app/services/factory/video_generator.py
+++ app/services/factory/video_generator.py
@@ -8,6 +8,8 @@ from moviepy.editor import *
 from moviepy.video.fx.resize import resize
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.editor import ImageClip
+
+from app.constant.video_const import PUNCTUATIONS
 from app.schemas.movies import VideoAspect, VideoConcatMode
 from app.services.redis_service import redis_service
 from app.utils import utils
@@ -216,17 +218,44 @@ def wrap_text(text, max_width, font="Arial", fontsize=60):
 def create_title_clip(params, title, video_width, video_height, font_path):
     """生成视频封面"""
     # 包装文本 字体显示的宽度和大小
-    width_factor = 0.4  # 字幕显示的区域占比视频画面宽度的比例
-    font_factor = 3  # 封面标题字体大小与字幕字体大小倍率
+    width_factor = 0.5  # 字幕显示的区域占比视频画面宽度的比例
+    font_size_factor = 2  # 封面标题字体大小与字幕字体大小倍率
     duration = 0.5  # 视频封面播放的时长秒
-    wrapped_title, _ = wrap_text(title, video_width * width_factor, font=font_path,
-                                 fontsize=params.font_size * font_factor)
+
+    def wrap_title_text(text, max_width, font_path, fontsize):
+        font = ImageFont.truetype(font_path, fontsize)
+
+        def get_text_size(inner_text):
+            inner_text = inner_text.strip()
+            left, top, right, bottom = font.getbbox(inner_text)
+            return right - left, bottom - top
+
+        lines = []
+        current_line = ""
+        for char in text:
+            # 如果标题中有中文标点符号就换行
+            if char in PUNCTUATIONS:
+                lines.append(current_line.strip())
+                current_line = ""
+            else:
+                current_line += char
+                width, _ = get_text_size(current_line)
+                if width > max_width:
+                    lines.append(current_line.strip())
+                    current_line = ""
+        if current_line:
+            lines.append(current_line.strip())
+
+        wrapped_text = "\n".join(lines)
+        return wrapped_text
+
+    wrapped_title = wrap_title_text(title, video_width * width_factor, font_path, params.font_size * font_size_factor)
 
     # 创建文字剪辑
     text_clip = TextClip(
         wrapped_title,
         font=font_path,
-        fontsize=params.font_size,
+        fontsize=params.font_size * font_size_factor,
         color=params.text_fore_color,
         bg_color=params.text_background_color,
         stroke_color=params.stroke_color,
