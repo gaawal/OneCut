c61a4b7d61d86170332a420c6d7716ce0ed9a46e
jiahualink <840132699@qq.com>
2024-08-07 07:38:23 +0800

    修复西瓜上传视频失败问题

--- app/utils/uploader/xigua_uploader/main.py
+++ app/utils/uploader/xigua_uploader/main.py
@@ -88,9 +88,9 @@ class XiguaVideo(object):
     async def upload(self, playwright: Playwright) -> None:
         # 使用 Chromium 浏览器启动一个浏览器实例
         if self.local_executable_path:
-            browser = await playwright.chromium.launch(headless=False, executable_path=self.local_executable_path)
+            browser = await playwright.chromium.launch(headless=True, executable_path=self.local_executable_path)
         else:
-            browser = await playwright.chromium.launch(headless=False)
+            browser = await playwright.chromium.launch(headless=True)
         # 创建一个浏览器上下文，使用指定的 cookie 文件
         context = await browser.new_context(storage_state=f"{self.account_file}")
         context = await set_init_script(context)
