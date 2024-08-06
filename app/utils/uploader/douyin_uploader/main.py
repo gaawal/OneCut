6e290b744d179598e3e7a2452d7214505c575afa
jiahualink <840132699@qq.com>
2024-08-07 07:35:53 +0800

    修复抖音上传视频失败问题

--- app/utils/uploader/douyin_uploader/main.py
+++ app/utils/uploader/douyin_uploader/main.py
@@ -107,7 +107,7 @@ class DouYinVideo(object):
         logger.info(f'[Douyin] 正在打开主页...')
         await page.wait_for_url("https://creator.douyin.com/creator-micro/content/upload")
         # 点击 "上传视频" 按钮
-        await page.locator(".upload-btn--9eZLd").set_input_files(self.file_path)
+        await page.locator('input[class^="upload-btn-input-"][type="file"]').set_input_files(self.file_path)
 
         # 等待页面跳转到指定的 URL
         while True:
--- app/utils/uploader/xigua_uploader/main.py
+++ app/utils/uploader/xigua_uploader/main.py
@@ -19,10 +19,10 @@ async def cookie_auth(account_file):
         # 访问指定的 URL
         await page.goto("https://studio.ixigua.com/upload")
         if await page.get_by_text('登录 / 注册').count():
-            print("[+] 等待5秒 cookie 失效")
+            print("[Xigua] 等待5秒 cookie 失效")
             return False
         else:
-            logger.success("[+] cookie 有效")
+            logger.success("[Xigua] cookie 有效")
             return True
 
 
@@ -31,7 +31,7 @@ async def xigua_setup(account_file, handle=False):
         if not handle:
             # Todo alert message
             return False
-        logger.info('[+] cookie文件不存在或已失效，即将自动打开浏览器，请扫码登录，登陆后会自动生成cookie文件')
+        logger.info('[Xigua] cookie文件不存在或已失效，即将自动打开浏览器，请扫码登录，登陆后会自动生成cookie文件')
         await xigua_cookie_gen(account_file)
     return True
 
@@ -99,9 +99,9 @@ class XiguaVideo(object):
         page = await context.new_page()
         # 访问指定的 URL
         await page.goto("https://studio.ixigua.com/upload")
-        logger.info(f'[+]正在上传-------{self.title}.mp4')
+        logger.info(f'[Xigua]正在上传-------{self.title}.mp4')
         # 等待页面跳转到指定的 URL，没进入，则自动等待到超时
-        logger.info(f'[-] 正在打开主页...')
+        logger.info(f'[Xigua] 正在打开主页...')
         await page.wait_for_url("https://studio.ixigua.com/upload")
         # 点击 "上传视频" 按钮
         await page.locator('input[type="file"]').set_input_files(self.file_path)
@@ -111,7 +111,7 @@ class XiguaVideo(object):
         await asyncio.sleep(1)
         # 填充标题和话题
         await asyncio.sleep(1)
-        logger.info(f'  [-] 正在填充标题和话题...')
+        logger.info(f'  [Xigua] 正在填充标题和话题...')
         title_container = page.locator('div.mentionText').first
         await title_container.click()
         await page.keyboard.press("Backspace")
@@ -129,38 +129,38 @@ class XiguaVideo(object):
         logger.info(f'总共添加{len(self.tags)}个话题')
 
         # 点击弹窗中的 "下一步" 按钮
-        logger.info('  [-] 正在勾选原创按钮...')
+        logger.info('  [Xigua] 正在勾选原创按钮...')
         next_button = page.locator('span.byte-radio-inner-text:has-text("原创")')
         await next_button.wait_for(timeout=5000)
         await next_button.click()
         await asyncio.sleep(2)
 
         # 点击 "上传封面" 按钮
-        logger.info('  [-] 正在点击上传封面按钮...')
+        logger.info('  [Xigua] 正在点击上传封面按钮...')
         await page.locator('text=上传封面').click()
         await asyncio.sleep(2)
 
         # 点击弹窗中的 "下一步" 按钮
-        logger.info('  [-] 正在点击弹窗中的本地上传...')
+        logger.info('  [Xigua] 正在点击弹窗中的本地上传...')
         next_button = page.locator('text=本地上传')
         await next_button.wait_for(timeout=5000)
         await next_button.click()
         await asyncio.sleep(2)
 
         # 点击弹窗中的 "下一步" 按钮
-        logger.info('  [-] 正在上传封面图片...')
+        logger.info('  [Xigua] 正在上传封面图片...')
         await page.locator('input[type="file"]').set_input_files(self.cover_image)
         await asyncio.sleep(2)
 
         # 点击提示框中的 "确定" 按钮
-        logger.info('  [-] 正在点击提示框中的确定按钮...')
+        logger.info('  [Xigua] 正在点击提示框中的确定按钮...')
         confirm_button = page.locator('text=确定')
         await confirm_button.wait_for()
         await confirm_button.click()
         await asyncio.sleep(2)
 
         # 点击提示框中的第二个 "确定" 按钮
-        logger.info('  [-] 正在点击提示框中的第二个确定按钮...')
+        logger.info('  [Xigua] 正在点击提示框中的第二个确定按钮...')
         confirm_button_red = page.locator('button.m-button.red').nth(1)
         await confirm_button_red.wait_for()
         await confirm_button_red.click()
@@ -173,17 +173,17 @@ class XiguaVideo(object):
                 #  新版：根据 class="success" 和文本内容 "上传成功" 进行判断
                 number = await page.locator('text=上传成功').count()
                 if number > 0:
-                    logger.success("  [-]视频上传完毕")
+                    logger.success("  [Xigua]视频上传完毕")
                     break
                 else:
-                    logger.info("  [-] 正在上传视频中...")
+                    logger.info("  [Xigua] 正在上传视频中...")
                     await asyncio.sleep(2)
 
                     if await page.locator('text=上传失败').count():
-                        logger.error("  [-] 发现上传出错了... 准备重试")
+                        logger.error("  [Xigua] 发现上传出错了... 准备重试")
                         await self.handle_upload_error(page)
             except:
-                logger.info("  [-] 正在上传视频中...")
+                logger.info("  [Xigua] 正在上传视频中...")
                 await asyncio.sleep(2)
 
 
@@ -200,15 +200,15 @@ class XiguaVideo(object):
                     pass
                 await page.wait_for_url("https://studio.ixigua.com/content**",
                                         timeout=1500)  # 如果自动跳转到作品页面，则代表发布成功
-                logger.success("  [-]视频发布成功")
+                logger.success("  [Xigua]视频发布成功")
                 break
             except:
-                logger.info("  [-] 视频正在发布中...")
+                logger.info("  [Xigua] 视频正在发布中...")
                 await page.screenshot(full_page=True)
                 await asyncio.sleep(0.5)
 
         await context.storage_state(path=self.account_file)  # 保存cookie
-        logger.success('  [-]cookie更新完毕！')
+        logger.success('  [Xigua]cookie更新完毕！')
         await asyncio.sleep(2)  # 这里延迟是为了方便眼睛直观的观看
         # 关闭浏览器上下文和浏览器实例
         await context.close()
